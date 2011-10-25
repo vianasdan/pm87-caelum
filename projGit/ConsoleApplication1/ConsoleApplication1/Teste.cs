1824663f0777a78413ef870e5e26e675194566a1
unknown <agil2414@.(none)>
2011-10-24 22:50:49 -0200

    Primeiro commit do projeto com classe de exemplo

---
+++ projGit/ConsoleApplication1/ConsoleApplication1/Teste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Teste
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de teste de uso do git");          
+        }
+    }
+}
---
+++ projGit/pm87-caelum/pm87-caelum/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pm87_caelum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
